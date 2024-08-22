dd2d7426fca018ae979887a7e860bd9613dcc891
Jet Taekyo Lee <jetleee.1888@gmail.com>
2024-08-23 07:27:51 +0900

    aaa

--- Week 1/Day 2/aimakerspace/text_utils.py
+++ Week 1/Day 2/aimakerspace/text_utils.py
@@ -1,9 +1,10 @@
 import os
 from typing import List
-import PyPDF2  # Added by me
+import PyPDF2  
+from langchain_text_splitters import RecursiveCharacterTextSplitter
  
 
-class TextFileLoader:
+class FileLoader:
     def __init__(self, path: str, encoding: str = "utf-8"):
         self.documents = []
         self.path = path
@@ -12,77 +13,31 @@ class TextFileLoader:
     def load(self):
         if os.path.isdir(self.path):
             self.load_directory()
-        elif os.path.isfile(self.path) and (self.path.endswith(".txt") or self.path.endswith(".pdf")): # Modified by me
-            self.load_file()
         else:
-            raise ValueError(
-                "Provided path is neither a valid directory nor a .txt/.pdf file."
-            )
+            self.load_file()
 
     def load_file(self):
-        # Modified by me
         if self.path.endswith(".txt"):
             with open(self.path, "r", encoding=self.encoding) as f:
                 self.documents.append(f.read())
-        else:
-            with open(self.path, 'rb') as f:
-                pdf_reader = PyPDF2.PdfReader(f)  
+        
+        elif self.path.endswith(".pdf"):
+            with open(self.path, "rb") as file:
+                reader = PyPDF2.PdfReader(file)
+                for i in range(len(reader.pages) ):
+                    pageObj = reader.pages[i]
+                    self.documents.append(pageObj.extract_text())
             
-            text = ""
-            for page_num in range(len(pdf_reader.pages)):
-                page = pdf_reader.pages[page_num]
-                self.documents.append(page.extract_text())
+            
+        else:
+            raise ValueError("Provided file is not a .txt file or a .pdf file.")
 
     def load_directory(self):
         for root, _, files in os.walk(self.path):
             for file in files:
-                if file.endswith(".txt"):
-                    with open(
-                        os.path.join(root, file), "r", encoding=self.encoding
-                    ) as f:
-                        self.documents.append(f.read())
+                if file.endswith(".txt") or file.endswith(".pdf"):
+                    self.load_file()                    
 
     def load_documents(self):
         self.load()
         return self.documents
-
-
-class CharacterTextSplitter:
-    def __init__(
-        self,
-        chunk_size: int = 1000,
-        chunk_overlap: int = 200,
-    ):
-        assert (
-            chunk_size > chunk_overlap
-        ), "Chunk size must be greater than chunk overlap"
-
-        self.chunk_size = chunk_size
-        self.chunk_overlap = chunk_overlap
-
-    def split(self, text: str) -> List[str]:
-        chunks = []
-        for i in range(0, len(text), self.chunk_size - self.chunk_overlap): # by default, it is range(0, len(text), 800)  
-            chunks.append(text[i : i + self.chunk_size])
-        return chunks
-
-    def split_texts(self, texts: List[str]) -> List[str]:
-        chunks = []
-        for text in texts:
-            chunks.extend(self.split(text))
-        return chunks
-
-
-if __name__ == "__main__":
-    loader = TextFileLoader("data/KingLear.txt")
-    loader.load()
-    splitter = CharacterTextSplitter()
-    chunks = splitter.split_texts(loader.documents)
-    print(len(chunks))
-    print(chunks[0])
-    print("--------")
-    print(chunks[1])
-    print("--------")
-    print(chunks[-2])
-    print("--------")
-    print(chunks[-1])
--- Week 1/Day 2/aimakerspace/vectordatabase.py
+++ Week 1/Day 2/aimakerspace/vectordatabase.py
@@ -12,16 +12,19 @@ def cosine_similarity(vector_a: np.array, vector_b: np.array) -> float:
     norm_b = np.linalg.norm(vector_b)
     return dot_product / (norm_a * norm_b)
 
-# Added by me
 def manhattan_distance(vector_a: np.array, vector_b: np.array) -> float:
     """Computes Manhattan Distance between two vectors."""
     distance = np.sum(np.abs(vector_a - vector_b))
     similarity = 1 / (1 + distance)
     return similarity
 
+def inner_product(vector_a: np.array, vector_b: np.array) -> float:
+    """Computes the inner product between two vectors."""
+    return np.dot(vector_a, vector_b)
+
 
 class VectorDatabase:
-    def __init__(self, embedding_model: EmbeddingModel = None):
+    def __init__(self, embedding_model: EmbeddingModel = None) -> None:
         self.vectors = defaultdict(np.array)
         self.embedding_model = embedding_model or EmbeddingModel()
 
@@ -34,10 +37,7 @@ class VectorDatabase:
         k: int,
         distance_measure: Callable = cosine_similarity,
     ) -> List[Tuple[str, float]]:
-        scores = [
-            (key, distance_measure(query_vector, vector))
-            for key, vector in self.vectors.items()
-        ]
+        scores = [ (text, distance_measure(query_vector, vector)) for text, vector in self.vectors.items() ]
         return sorted(scores, key=lambda x: x[1], reverse=True)[:k]
 
     def search_by_text(
@@ -47,16 +47,17 @@ class VectorDatabase:
         distance_measure: Callable = cosine_similarity,
         return_as_text: bool = False,
     ) -> List[Tuple[str, float]]:
-        query_vector = self.embedding_model.get_embedding(query_text)
+        query_vector = self.embedding_model.get_embedding(query_text)        
         results = self.search(query_vector, k, distance_measure)
         return [result[0] for result in results] if return_as_text else results
 
-    def retrieve_from_key(self, key: str) -> np.array:
-        return self.vectors.get(key, None)
+    def retrieve_from_text(self, text: str) -> np.array:
+        return self.vectors.get(text, None)
 
-    async def abuild_from_list(self, list_of_text: List[str]) -> "VectorDatabase":
-        embeddings = await self.embedding_model.async_get_embeddings(list_of_text)
-        for text, embedding in zip(list_of_text, embeddings):
+    async def abuild_from_list(self, list_of_texts: List[str]) -> 'VectorDatabase':
+        embeddings = await self.embedding_model.async_get_embeddings(list_of_texts)
+        
+        for text, embedding in zip(list_of_texts, embeddings):
             self.insert(text, np.array(embedding))
         return self
 
