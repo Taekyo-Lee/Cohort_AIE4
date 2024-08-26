41575a1d9f387d952555ef1ad977f29158450121
Jet Taekyo Lee <jetleee.1888@gmail.com>
2024-08-26 16:31:01 +0900

    aie4 assignment 2

--- Week 1/Day 2/aimakerspace/text_utils.py
+++ Week 1/Day 2/aimakerspace/text_utils.py
@@ -1,10 +1,8 @@
 import os
 from typing import List
-import PyPDF2  
-from langchain_text_splitters import RecursiveCharacterTextSplitter
- 
 
-class FileLoader:
+
+class TextFileLoader:
     def __init__(self, path: str, encoding: str = "utf-8"):
         self.documents = []
         self.path = path
@@ -13,31 +11,67 @@ class FileLoader:
     def load(self):
         if os.path.isdir(self.path):
             self.load_directory()
-        else:
+        elif os.path.isfile(self.path) and self.path.endswith(".txt"):
             self.load_file()
+        else:
+            raise ValueError(
+                "Provided path is neither a valid directory nor a .txt file."
+            )
 
     def load_file(self):
-        if self.path.endswith(".txt"):
-            with open(self.path, "r", encoding=self.encoding) as f:
-                self.documents.append(f.read())
-        
-        elif self.path.endswith(".pdf"):
-            with open(self.path, "rb") as file:
-                reader = PyPDF2.PdfReader(file)
-                for i in range(len(reader.pages) ):
-                    pageObj = reader.pages[i]
-                    self.documents.append(pageObj.extract_text())
-            
-            
-        else:
-            raise ValueError("Provided file is not a .txt file or a .pdf file.")
+        with open(self.path, "r", encoding=self.encoding) as f:
+            self.documents.append(f.read())
 
     def load_directory(self):
         for root, _, files in os.walk(self.path):
             for file in files:
-                if file.endswith(".txt") or file.endswith(".pdf"):
-                    self.load_file()                    
+                if file.endswith(".txt"):
+                    with open(
+                        os.path.join(root, file), "r", encoding=self.encoding
+                    ) as f:
+                        self.documents.append(f.read())
 
     def load_documents(self):
         self.load()
         return self.documents
+
+
+class CharacterTextSplitter:
+    def __init__(
+        self,
+        chunk_size: int = 1000,
+        chunk_overlap: int = 200,
+    ):
+        assert (
+            chunk_size > chunk_overlap
+        ), "Chunk size must be greater than chunk overlap"
+
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+
+    def split(self, text: str) -> List[str]:
+        chunks = []
+        for i in range(0, len(text), self.chunk_size - self.chunk_overlap):
+            chunks.append(text[i : i + self.chunk_size])
+        return chunks
+
+    def split_texts(self, texts: List[str]) -> List[str]:
+        chunks = []
+        for text in texts:
+            chunks.extend(self.split(text))
+        return chunks
+
+
+if __name__ == "__main__":
+    loader = TextFileLoader("data/KingLear.txt")
+    loader.load()
+    splitter = CharacterTextSplitter()
+    chunks = splitter.split_texts(loader.documents)
+    print(len(chunks))
+    print(chunks[0])
+    print("--------")
+    print(chunks[1])
+    print("--------")
+    print(chunks[-2])
+    print("--------")
+    print(chunks[-1])
