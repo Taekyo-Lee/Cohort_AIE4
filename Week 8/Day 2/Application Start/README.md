ae36d0d75ff313853d3fe75ca9dc88680b76a84d
Jet Taekyo Lee <jetleee.1888@gmail.com>
2024-10-07 23:29:40 +0900

    aaa

---
+++ Week 8/Day 2/Application Start/README.md
@@ -0,0 +1,11 @@
+---
+title: AIE4 LCEL WITH HFI (AIE4 WEEK8 DAY2)
+emoji: 🚀
+colorFrom: blue
+colorTo: purple
+sdk: docker
+pinned: false
+license: mit
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
--- Week 8/Day 2/Application Start/app.py
+++ Week 8/Day 2/Application Start/app.py
@@ -17,16 +17,7 @@ from tqdm.asyncio import tqdm
 
 # GLOBAL SCOPE - ENTIRE APPLICATION HAS ACCESS TO VALUES SET IN THIS SCOPE #
 # ---- ENV VARIABLES ---- # 
-"""
-This function will load our environment file (.env) if it is present.
-
-NOTE: Make sure that .env is in your .gitignore file - it is by default, but please ensure it remains there.
-"""
 load_dotenv()
-
-"""
-We will load our environment variables here.
-"""
 HF_LLM_ENDPOINT = os.environ["HF_LLM_ENDPOINT"]
 HF_EMBED_ENDPOINT = os.environ["HF_EMBED_ENDPOINT"]
 HF_TOKEN = os.environ["HF_TOKEN"]
@@ -42,15 +33,21 @@ HF_TOKEN = os.environ["HF_TOKEN"]
 """
 ### 1. CREATE TEXT LOADER AND LOAD DOCUMENTS
 ### NOTE: PAY ATTENTION TO THE PATH THEY ARE IN. 
-text_loader = 
-documents = 
+text_loader = TextLoader("data/paul_graham_essays.txt")
+documents = text_loader.load()
+
 
 ### 2. CREATE TEXT SPLITTER AND SPLIT DOCUMENTS
-text_splitter = 
-split_documents = 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=30)
+split_documents = text_splitter.split_documents(documents)
 
 ### 3. LOAD HUGGINGFACE EMBEDDINGS
-hf_embeddings = 
+hf_embeddings = HuggingFaceEndpointEmbeddings(
+    model=HF_EMBED_ENDPOINT,
+    task="feature-extraction",
+    huggingfacehub_api_token=HF_TOKEN,
+)
+
 
 async def add_documents_async(vectorstore, documents):
     await vectorstore.aadd_documents(documents)
@@ -63,6 +60,7 @@ async def process_batch(vectorstore, batch, is_first_batch, pbar):
         result = vectorstore
     pbar.update(len(batch))
     return result
+
 
 async def main():
     print("Indexing Files")
@@ -102,7 +100,8 @@ async def run():
     retriever = await main()
     return retriever
 
-hf_retriever = asyncio.run(run())
+hf_retriever = asyncio.run(run())  # hf_retriever is a Runnable so it supports async operations 
+print("😉 Retriever is ready 😉")
 
 # -- AUGMENTED -- #
 """
@@ -110,17 +109,38 @@ hf_retriever = asyncio.run(run())
 2. Create a Prompt Template from the String Template
 """
 ### 1. DEFINE STRING TEMPLATE
-RAG_PROMPT_TEMPLATE = 
+RAG_PROMPT_TEMPLATE = """\
+<|start_header_id|>system<|end_header_id|>
+You are a helpful assistant. You answer user questions based on provided context. If you can't answer the question with the provided context, say you don't know.<|eot_id|>
+
+<|start_header_id|>user<|end_header_id|>
+User Query:
+{query}
+
+Context:
+{context}<|eot_id|>
+
+<|start_header_id|>assistant<|end_header_id|>
+"""
 
 ### 2. CREATE PROMPT TEMPLATE
-rag_prompt =
+rag_prompt = PromptTemplate.from_template(RAG_PROMPT_TEMPLATE) 
 
 # -- GENERATION -- #
 """
 1. Create a HuggingFaceEndpoint for the LLM
 """
 ### 1. CREATE HUGGINGFACE ENDPOINT FOR LLM
-hf_llm = 
+hf_llm = HuggingFaceEndpoint(
+    endpoint_url=HF_LLM_ENDPOINT,
+    max_new_tokens=512,
+    top_k=10,
+    top_p=0.95,
+    typical_p=0.95,
+    temperature=0.01,
+    repetition_penalty=1.03,
+    huggingfacehub_api_token=HF_TOKEN
+)
 
 @cl.author_rename
 def rename(original_author: str):
@@ -145,7 +165,11 @@ async def start_chat():
     """
 
     ### BUILD LCEL RAG CHAIN THAT ONLY RETURNS TEXT
-    lcel_rag_chain = 
+    lcel_rag_chain =  (  
+            RunnablePassthrough.assign(context = itemgetter("query") | hf_retriever ).with_config({'run_name':'SNOWFLAKE_RETRIEVER'}) | 
+            rag_prompt.with_config({'run_name':'LLAMA3_PROMPT'}) | 
+            hf_llm.with_config({'run_name':'LLAMA3'})
+        ).with_config({"run_name":"HFI_RAG"})
 
     cl.user_session.set("lcel_rag_chain", lcel_rag_chain)
 
@@ -166,6 +190,6 @@ async def main(message: cl.Message):
         {"query": message.content},
         config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
     ):
-        await msg.stream_token(chunk)
+        await msg.stream_token(chunk) if chunk != '<|eot_id|>' else await msg.stream_token('')
 
     await msg.send()
